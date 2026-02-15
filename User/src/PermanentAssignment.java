c376530085da015ca9ded47fabdbe1493c763435
devl2 <bokiy05@mail.ru>
2026-02-16 00:04:59 +0700

    dev: create class Permanent- and Temporary-Assignment

---
+++ User/src/PermanentAssignment.java
@@ -0,0 +1,27 @@
+public class PermanentAssignment extends AbstractRoleAssignment {
+
+    private boolean revoked;
+
+    public PermanentAssignment(User user, Role role, AssignmentMetadata metadata) {
+        super(user, role, metadata);
+        this.revoked = false;
+    }
+
+    @Override
+    public String assignmentType() {
+        return "PERMANENT";
+    }
+
+    @Override
+    public boolean isActive() {
+        return !revoked;
+    }
+
+    public void revoke() {
+        this.revoked = true;
+    }
+
+    public boolean isRevoked() {
+        return revoked;
+    }
+}
