bca76a3aa250ef8333825f1e6a245e83ed9a06ab
devl2 <ip314s04@lwp08.ipa.csc.sibsutis.ru>
2026-02-18 05:12:24 +0700

    dev: create new interface and methods RoleFilters

---
+++ User/src/RoleFilter.java
@@ -0,0 +1,7 @@
+@FunctionalInterface
+public interface RoleFilter {
+    boolean test (Role role);
+
+    default RoleFilter and(RoleFilter other) {return role -> this.test(role) && other.test(role);}
+    default RoleFilter or(RoleFilter other) {return role -> this.test(role) || other.test(role);}
+}
