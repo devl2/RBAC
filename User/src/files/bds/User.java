591127144299bf547bd0dfa13bf175ee37f6e6b9
devl2 <ip314s04@lwp08.ipa.csc.sibsutis.ru>
2026-03-09 10:39:54 +0700

    refactor: changed validation method at create user

--- User/src/files/bds/User.java
+++ User/src/files/bds/User.java
@@ -1,26 +1,16 @@
 package bds;
+import util.ValidationUtils;
 
 public record User(String username, String fullname, String email){
     public static User create(String username, String fullname, String email){
-        if(username == null || username.isBlank()){
-            throw new IllegalArgumentException("Строка не может быть пустой или null");
-        }
-        if(fullname == null || fullname.isBlank()){
-            throw new IllegalArgumentException("Строка не может быть пустой или null");
-        }
-        if(email == null || email.isBlank()){
-            throw new IllegalArgumentException("Строка не может быть пустой или null");
-        }
-        if(!username.matches("^[a-zA-Z0-9_]+$")){
-            throw new IllegalArgumentException("Поле username должен содержать только латинские буквы, цифры и подчёркивание");
-        }
-        if(username.length() < 3 || username.length() > 20){
-            throw new IllegalArgumentException("Поле username должен быть от 3 до 20 символов");
-        }
-        if (!email.matches("^[a-zA-Z0-9_]+@?[a-zA-Z]+\\.[a-zA-Z]+$")) {
+        ValidationUtils.requireNonEmpty(fullname, "fullname");
+
+        if (!ValidationUtils.isValidEmail(email)){
             throw new IllegalArgumentException("Введен неверный email");
         }
-
+        if (!ValidationUtils.isValidUsername(username)){
+            throw new IllegalArgumentException("Поле username должен содержать только латинские буквы, цифры и подчёркивание");
+        }
         return new User(username, fullname, email);
     }
 
